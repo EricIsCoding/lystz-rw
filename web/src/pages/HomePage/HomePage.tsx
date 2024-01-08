350cbbb877add181280482e6da1a0c99ce2bc372
ericjwilhite <ericjwilhite@gmail.com>
2024-01-07 17:33:49 -0800

    Add Navigation Button

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -8,11 +8,28 @@ const HomePage = () => {
     <>
       {isAuthenticated ? (
         <div>
-          <h1>Welcome to My App!</h1>
-          <p>This is a simple landing page for the RedwoodJS App.</p>
+          <header className="rw-header">
+            <h1 className="rw-heading rw-heading-primary">Lystz</h1>
+            <Link
+              className={'rw-button rw-button-green'}
+              to={routes.todoLists()}
+            >
+              <div className="rw-button-icon"> {'->'} </div> Todo Lists!
+            </Link>
+            <Link
+              className={'rw-button rw-button-green'}
+              to={routes.todoItems()}
+            >
+              <div className="rw-button-icon"> {'->'} </div> Todo Items
+            </Link>
+          </header>
+          <p>
+            This is a simple landing page for the Lystz App. Please navigate
+            using links above.
+          </p>
           {/* Add navigation or call-to-action buttons */}
           <p>You are logged in!</p>
-          <button onClick={logOut}>Logout</button>
+          <button className={'rw-button'} onClick={logOut}>Logout</button>
           {/* Add any additional sections or content */}
         </div>
       ) : (
