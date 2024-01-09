f3f68f828f62645b6a01835ced01e63b9d8b6e35
ericjwilhite <ericjwilhite@gmail.com>
2024-01-08 17:13:28 -0800

    Prettify Scaffold Classes

--- web/src/layouts/ScaffoldLayout/ScaffoldLayout.tsx
+++ web/src/layouts/ScaffoldLayout/ScaffoldLayout.tsx
@@ -19,8 +19,8 @@ const ScaffoldLayout = ({
   return (
     <div className="min-h-screen bg-gray-100">
       <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
-      <header className="bg-white shadow py-6">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center">
+      <header className="bg-white py-6 shadow">
+        <div className="mx-auto flex max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
           <h1 className="text-2xl font-semibold text-gray-800">
             <Link
               to={routes[titleTo]()}
@@ -32,13 +32,13 @@ const ScaffoldLayout = ({
           <div className="flex gap-4">
             <Link
               to={routes.home()}
-              className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-teal-600 hover:bg-teal-900"
+              className="inline-flex items-center rounded-md border border-transparent bg-teal-600 px-4 py-2 text-sm font-medium text-white hover:bg-teal-900"
             >
               Go Home!
             </Link>
             <Link
               to={routes[buttonTo]()}
-              className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-teal-600 hover:bg-teal-900"
+              className="inline-flex items-center rounded-md border border-transparent bg-teal-600 px-4 py-2 text-sm font-medium text-white hover:bg-teal-900"
             >
               <span className="mr-2">+</span> {buttonLabel}
             </Link>
@@ -46,7 +46,7 @@ const ScaffoldLayout = ({
         </div>
       </header>
       <main className="py-6">
-        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">{children}</div>
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">{children}</div>
       </main>
     </div>
   )
