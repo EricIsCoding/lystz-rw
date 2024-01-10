d11d00bab193ee6f4d1b5bef241719ac3f9d0500
ericjwilhite <ericjwilhite@gmail.com>
2024-01-09 21:32:57 -0800

    Removed a lot of unused code from TodoLists

--- web/src/components/TodoList/TodoLists/TodoLists.tsx
+++ web/src/components/TodoList/TodoLists/TodoLists.tsx
@@ -1,38 +1,8 @@
-import type {
-  DeleteTodoListMutationVariables,
-  FindTodoLists,
-} from 'types/graphql'
-
-import { useMutation } from '@redwoodjs/web'
-import { toast } from '@redwoodjs/web/toast'
-
-import { QUERY } from 'src/components/TodoList/TodoListsCell'
+import type { FindTodoLists } from 'types/graphql'
 
 import TodoListCard from '../TodoListCard/TodoListCard'
 
-const DELETE_TODO_LIST_MUTATION = gql`
-  mutation DeleteTodoListMutation($id: Int!) {
-    deleteTodoList(id: $id) {
-      id
-    }
-  }
-`
-
 const TodoListsList = ({ todoLists }: FindTodoLists) => {
-  const [deleteTodoList] = useMutation(DELETE_TODO_LIST_MUTATION, {
-    onCompleted: () => {
-      toast.success('TodoList deleted')
-    },
-    onError: (error) => {
-      toast.error(error.message)
-    },
-    // This refetches the query on the list page. Read more about other ways to
-    // update the cache over here:
-    // https://www.apollographql.com/docs/react/data/mutations/#making-all-other-cache-updates
-    refetchQueries: [{ query: QUERY }],
-    awaitRefetchQueries: true,
-  })
-
   return (
     <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
       <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
