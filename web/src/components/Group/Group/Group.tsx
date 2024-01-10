5d89335c3f8db2169470e30221d594e8d436acc3
ericjwilhite <ericjwilhite@gmail.com>
2024-01-09 21:35:47 -0800

    Updated the styling on the Groups details page

--- web/src/components/Group/Group/Group.tsx
+++ web/src/components/Group/Group/Group.tsx
@@ -1,10 +1,10 @@
+import type { DeleteGroupMutationVariables, FindGroupById } from 'types/graphql'
+
 import { Link, routes, navigate } from '@redwoodjs/router'
 import { useMutation } from '@redwoodjs/web'
 import { toast } from '@redwoodjs/web/toast'
 
-import { timeTag } from 'src/lib/formatters'
-
-import type { DeleteGroupMutationVariables, FindGroupById } from 'types/graphql'
+import { timeTag, truncate } from 'src/lib/formatters'
 
 const DELETE_GROUP_MUTATION = gql`
   mutation DeleteGroupMutation($id: Int!) {
@@ -37,43 +37,48 @@ const Group = ({ group }: Props) => {
 
   return (
     <>
-      <div className="rw-segment">
-        <header className="rw-segment-header">
-          <h2 className="rw-heading rw-heading-secondary">
-            Group {group.id} Detail
+      <div className="m-1 overflow-hidden rounded-lg bg-white shadow-md">
+        <header className="bg-gray-50 px-4 py-5 sm:px-6">
+          <h2 className="text-xl font-semibold leading-6 text-gray-800">
+            {truncate(group.name)}
           </h2>
         </header>
-        <table className="rw-table">
-          <tbody>
-            <tr>
-              <th>Id</th>
-              <td>{group.id}</td>
-            </tr>
-            <tr>
-              <th>Name</th>
-              <td>{group.name}</td>
-            </tr>
-            <tr>
-              <th>Description</th>
-              <td>{group.description}</td>
-            </tr>
-            <tr>
-              <th>Created at</th>
-              <td>{timeTag(group.createdAt)}</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="border-t border-gray-200">
+          <dl>
+            {/* Group Description */}
+            <div className="grid grid-cols-1 gap-4 bg-white px-4 py-5 sm:px-6 md:grid-cols-3">
+              <dt className="text-sm font-semibold text-gray-600">
+                Description
+              </dt>
+              <dd className="mt-1 text-sm text-gray-900 md:col-span-2 md:mt-0">
+                {group.description}
+              </dd>
+            </div>
+
+            {/* Created At */}
+            <div className="grid grid-cols-1 gap-4 bg-gray-50 px-4 py-5 sm:px-6 md:grid-cols-3">
+              <dt className="text-sm font-semibold text-gray-600">
+                Created at
+              </dt>
+              <dd className="mt-1 text-sm text-gray-900 md:col-span-2 md:mt-0">
+                {timeTag(group.createdAt)}
+              </dd>
+            </div>
+          </dl>
+        </div>
       </div>
-      <nav className="rw-button-group">
+
+      {/* Action Buttons */}
+      <nav className="mt-4 flex items-center justify-between">
         <Link
           to={routes.editGroup({ id: group.id })}
-          className="rw-button rw-button-blue"
+          className="inline-flex items-center rounded-md border border-transparent bg-teal-600 px-4 py-2 text-sm font-medium text-white transition duration-150 ease-in-out hover:bg-blue-700"
         >
           Edit
         </Link>
         <button
           type="button"
-          className="rw-button rw-button-red"
+          className="inline-flex items-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-sm font-medium text-white transition duration-150 ease-in-out hover:bg-red-700"
           onClick={() => onDeleteClick(group.id)}
         >
           Delete
