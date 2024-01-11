783c6fe18a737478b5d2bfb117972f0770f7c58e
ericjwilhite <ericjwilhite@gmail.com>
2024-01-10 21:33:45 -0800

    Add PrivateSet component to Routes.tsx

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,7 +7,7 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Set, Router, Route } from '@redwoodjs/router'
+import { PrivateSet, Set, Router, Route } from '@redwoodjs/router'
 
 import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
 
@@ -16,31 +16,35 @@ import { useAuth } from './auth'
 const Routes = () => {
   return (
     <Router useAuth={useAuth}>
-      <Set wrap={ScaffoldLayout} title="Groups" titleTo="groups" buttonLabel="New Group" buttonTo="newGroup">
-        <Route path="/groups/new" page={GroupNewGroupPage} name="newGroup" />
-        <Route path="/groups/{id:Int}/edit" page={GroupEditGroupPage} name="editGroup" />
-        <Route path="/groups/{id:Int}" page={GroupGroupPage} name="group" />
-        <Route path="/groups" page={GroupGroupsPage} name="groups" />
-      </Set>
-      <Set wrap={ScaffoldLayout} title="Users" titleTo="users" buttonLabel="New User" buttonTo="newUser">
-        <Route path="/users/new" page={UserNewUserPage} name="newUser" />
-        <Route path="/users/{id:Int}/edit" page={UserEditUserPage} name="editUser" />
-        <Route path="/users/{id:Int}" page={UserUserPage} name="user" />
-        <Route path="/users" page={UserUsersPage} name="users" />
-      </Set>
-      <Set wrap={ScaffoldLayout} title="TodoItems" titleTo="todoItems" buttonLabel="New TodoItem" buttonTo="newTodoItem">
-        <Route path="/todo-items/new" page={TodoItemNewTodoItemPage} name="newTodoItem" />
-        <Route path="/todo-items/{id:Int}/edit" page={TodoItemEditTodoItemPage} name="editTodoItem" />
-        <Route path="/todo-items/{id:Int}" page={TodoItemTodoItemPage} name="todoItem" />
-        <Route path="/todo-items" page={TodoItemTodoItemsPage} name="todoItems" />
-      </Set>
-      <Set wrap={ScaffoldLayout} title="TodoLists" titleTo="todoLists" buttonLabel="New TodoList" buttonTo="newTodoList">
-        <Route path="/todo-lists/new" page={TodoListNewTodoListPage} name="newTodoList" />
-        <Route path="/todo-lists/{id:Int}/edit" page={TodoListEditTodoListPage} name="editTodoList" />
-        <Route path="/todo-lists/{id:Int}" page={TodoListTodoListPage} name="todoList" />
-        <Route path="/todo-lists" page={TodoListTodoListsPage} name="todoLists" />
-      </Set>
-      <Route path="/" page={HomePage} name="home" />
+      <PrivateSet unauthenticated="login">
+        <Set wrap={ScaffoldLayout} title="Groups" titleTo="groups" buttonLabel="New Group" buttonTo="newGroup">
+          <Route path="/groups/new" page={GroupNewGroupPage} name="newGroup" />
+          <Route path="/groups/{id:Int}/edit" page={GroupEditGroupPage} name="editGroup" />
+          <Route path="/groups/{id:Int}" page={GroupGroupPage} name="group" />
+          <Route path="/groups" page={GroupGroupsPage} name="groups" />
+        </Set>
+        <Set wrap={ScaffoldLayout} title="Users" titleTo="users" buttonLabel="New User" buttonTo="newUser">
+          <Route path="/users/new" page={UserNewUserPage} name="newUser" />
+          <Route path="/users/{id:Int}/edit" page={UserEditUserPage} name="editUser" />
+          <Route path="/users/{id:Int}" page={UserUserPage} name="user" />
+          <Route path="/users" page={UserUsersPage} name="users" />
+        </Set>
+        <Set wrap={ScaffoldLayout} title="Items" titleTo="todoItems" buttonLabel="New Item" buttonTo="newTodoItem">
+          <Route path="/todo-items/new" page={TodoItemNewTodoItemPage} name="newTodoItem" />
+          <Route path="/todo-items/{id:Int}/edit" page={TodoItemEditTodoItemPage} name="editTodoItem" />
+          <Route path="/todo-items/{id:Int}" page={TodoItemTodoItemPage} name="todoItem" />
+          <Route path="/todo-items" page={TodoItemTodoItemsPage} name="todoItems" />
+        </Set>
+        <Set wrap={ScaffoldLayout} title="Lists" titleTo="todoLists" buttonLabel="New List" buttonTo="newTodoList">
+          <Route path="/todo-lists/new" page={TodoListNewTodoListPage} name="newTodoList" />
+          <Route path="/todo-lists/{id:Int}/edit" page={TodoListEditTodoListPage} name="editTodoList" />
+          <Route path="/todo-lists/{id:Int}" page={TodoListTodoListPage} name="todoList" />
+          <Route path="/todo-lists" page={TodoListTodoListsPage} name="todoLists" />
+        </Set>
+        <Set wrap={ScaffoldLayout} title="Home Page" titleTo="home" buttonTo="home">
+          <Route path="/" page={HomePage} name="home" />
+        </Set>
+      </PrivateSet>
       <Route path="/login" page={LoginPage} name="login" />
       <Route path="/signup" page={SignupPage} name="signup" />
       <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
