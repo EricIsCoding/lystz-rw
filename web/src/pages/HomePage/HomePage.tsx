782584a28d52cdc1df0a9d98864181f95968f3bb
ericjwilhite <ericjwilhite@gmail.com>
2024-01-11 23:51:49 -0800

    Add StoresCell component to HomePage

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,17 @@
 import { Link, routes } from '@redwoodjs/router'
 
+import StoresCell from 'src/components/Store/StoresCell'
 import TodoListsCell from 'src/components/TodoList/TodoListsCell'
+
 const HomePage = () => {
   return (
-    <main className="m-1 py-6">
+    <main className="m-1 grid gap-5 py-6">
+      <div className="mx-auto max-w-7xl rounded-lg bg-white p-4 shadow sm:px-6 lg:px-8">
+        <h2 className="mb-4 text-2xl font-semibold text-gray-800 hover:text-teal-600">
+          <Link to={routes.stores()}>Your Stores!</Link>
+        </h2>
+        <StoresCell />
+      </div>
       <div className="mx-auto max-w-7xl rounded-lg bg-white p-4 shadow sm:px-6 lg:px-8">
         <h2 className="mb-4 text-2xl font-semibold text-gray-800 hover:text-teal-600">
           <Link to={routes.todoLists()}>Your Lists!</Link>
