a023923565f4f049207113fa12768b7ed2db8e75
ericjwilhite <ericjwilhite@gmail.com>
2024-01-11 21:13:54 -0800

    Refactor TodoLists component

--- web/src/components/TodoList/TodoLists/TodoLists.tsx
+++ web/src/components/TodoList/TodoLists/TodoLists.tsx
@@ -1,15 +1,9 @@
-import type { FindTodoLists } from 'types/graphql'
+import TodoListsCell from 'src/components/TodoList/TodoListsCell'
 
-import TodoListCard from '../TodoListCard/TodoListCard'
-
-const TodoListsList = ({ todoLists }: FindTodoLists) => {
+const TodoListsList = () => {
   return (
     <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        {todoLists.map((todoList) => (
-          <TodoListCard todoList={todoList} key={todoList.id} />
-        ))}
-      </div>
+      <TodoListsCell />
     </div>
   )
 }
